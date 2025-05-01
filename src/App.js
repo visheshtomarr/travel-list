5ef2fca2a5a0f31acbd6bccee92fef8021dd275b
visheshtomarr <visheshtomar7800@gmail.com>
2025-05-01 19:32:42 +0530

    Rendered items list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Shirts", quantity: 4, packed: false },
 ];
 
 function App() {
@@ -27,7 +28,15 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item =>
+          <Item item={item} key={item.id} />
+        )}
+      </ul>
+    </div>
+  )
 }
 
 function Stats() {
@@ -35,6 +44,17 @@ function Stats() {
     <footer className="stats">
       <em>You have X items on your list, and already packed X (X%)</em>
     </footer>
+  )
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   )
 }
 
