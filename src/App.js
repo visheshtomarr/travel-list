260117985a318e10e0d02cdbecf8a2de59892955
visheshtomarr <visheshtomar7800@gmail.com>
2025-05-01 19:11:49 +0530

    Built app layout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,37 @@ const initialItems = [
 
 function App() {
   return (
-    <div className="App">
-      Travel list
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
+}
+
+function Logo() {
+  return <h1>Let's Pack</h1>
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  )
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and already packed X (X%)</em>
+    </footer>
+  )
 }
 
 export default App;
