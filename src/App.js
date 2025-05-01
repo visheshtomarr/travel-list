30b8ce494256a767cd61794a09cb60deade3162c
visheshtomarr <visheshtomar7800@gmail.com>
2025-05-01 23:25:34 +0530

    Added form and state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,10 +22,46 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(2);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Adding guard clause.
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false
+    }
+
+    setDescription('');
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        {/* Creating an array of length 20 */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map
+          (num => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   )
 }
 
